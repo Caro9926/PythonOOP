8bf8979201d81c7f5ba359e164dc3fe6e2403ef8
Caro9926 <carolinasaavedra01@gmail.com>
2022-01-20 06:47:08 -0500

    encadenamiento de métodos

--- Usuario.py
+++ Usuario.py
@@ -7,15 +7,21 @@ class Usuario:
         self.balance_cuenta = 0
     
     def hacer_depósito(self, amount):	
-    	self.balance_cuenta += amount	
+        self.balance_cuenta += amount	
+        return self 
+          
+
     def hacer_retiro(self, amount):
         if self.balance_cuenta >= amount:
             self.balance_cuenta -= amount
         else:
             print(f"You don't have enough money for retry {self.name}")
-    
+        return self
+
     def mostrar_balance(self):
         print(f"Mr. {self.name} Your balance in the account is  {self.balance_cuenta}")
+        return self
+           
     
 
 guido = Usuario("Guido van Rossum", "guido@python.com")
@@ -23,19 +29,9 @@ monty = Usuario("Monty Python", "monty@python.com")
 pablo = Usuario("Pablo Ferreyros", "pablo@python.com")
 
 
-guido.hacer_depósito(100)
-guido.hacer_depósito(200)
-guido.hacer_depósito(300)
-guido.hacer_retiro(200)
-guido.mostrar_balance()
+guido.hacer_depósito(100).hacer_depósito(200).hacer_depósito(300).hacer_retiro(50).mostrar_balance()
+monty.hacer_depósito(50).hacer_depósito(100).hacer_retiro(180).mostrar_balance()
+pablo.hacer_depósito(300).hacer_retiro(20).hacer_retiro(90).hacer_retiro(34).mostrar_balance()
 
-monty.hacer_depósito(50)
-monty.hacer_depósito(100)
-monty.hacer_retiro(180)
-monty.mostrar_balance()
 
-pablo.hacer_depósito(300)
-pablo.hacer_retiro(20)
-pablo.hacer_retiro(90)
-pablo.hacer_retiro(34)
-pablo.mostrar_balance()
+
---
+++ main.py
@@ -0,0 +1,11 @@
+from Usuario import Usuario 
+
+a1= Usuario()
+a2= Usuario()
+
+a1.hacer_depósito(1000)
+a1.hacer_retiro(200)
+a2.hacer_retiro(20)
+a1.hacer_retiro(30)
+
+
